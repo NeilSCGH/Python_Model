68c113e9880f8f0f38e72e051ae9406532573c90
NeilSCGH <neil.segard@gmail.com>
2020-04-30 18:45:59 +0200

    maj

--- run function with parameters/lib/person.py
+++ run function with parameters/lib/person.py
@@ -1,34 +1,26 @@
 from lib.tools import *
 
 class person():
+
   def __init__(self,args):
+    self.tool = tools(args)
     self.firstName = "firstName"
     self.lastName = "lastName"
     self.age = "age"
     self.setup(args)
 
   def setup(self,args):
-    tool = tools(args)
-    if tool.argHasValue("-f"):
-      val=tool.argValue("-f")
+    if self.tool.argHasValue("-f"):
+      val=self.tool.argValue("-f")
       self.firstName=val
 
-    if tool.argHasValue("-l"):
-      val=tool.argValue("-l")
+    if self.tool.argHasValue("-l"):
+      val=self.tool.argValue("-l")
       self.lastName=val
-      
-    if tool.argHasValue("-a"):
-      val=tool.argValue("-a")
+
+    if self.tool.argHasValue("-a"):
+      val=self.tool.argValue("-a")
       self.age=val
-
-  def setFirstName(self,firstName):
-    self.firstName=firstName
-
-  def setLastName(self,lastName):
-  	self.lastName=lastName
-
-  def setAge(self,age):
-  	self.age=age
 
   def aff(self):
     print("{} {} : {}".format(self.firstName,self.lastName,self.age))
@@ -36,4 +28,8 @@ class person():
   def run(self):
     print("Current settings for {} :".format(self.firstName))
     self.aff()
+
+    if self.tool.argExist("-h"):
+      print("hello",self.firstName)
+
     print("Running !")
